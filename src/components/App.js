d057887b771f6ad0a9242741c540621bb0716cae
Barry <Barry-B15@users.noreply.github.com>
2021-07-11 00:13:18 +0900

    refactor: removing callback

--- src/components/App.js
+++ src/components/App.js
@@ -4,43 +4,35 @@ import VideoList from './VideoList'
 import VideoDetail from './VideoDetail'
 import youtube from '../apis/youtube'
 
-// 2. Declare a new App func
+
 const App = () => {
-    // 3. Declare 2 pieces of state corresponding to the params in the old App state {video:[], selectedVideo:null}
+   
     const [videos, setVideos] = useState([]);
     const [selectedVideo, setSelectedVideo] = useState(null);
 
-    // 4 declare a useEffect with an empty array as this func will run only once when the app 1st loads. Then cut n paste the content of componentDidMount. Note useEffect n componentDidMount are equivalent. they handle our app life cycle. make sure to remove this. in front of ontermSubmit
+    
     useEffect(() => {
         onTermSubmit('beautiful japan')
     }, []);
 
-    //5. cut a paste the onTermSubmit callback here, add const in front of onTermSubmit to make it a func, check n remove any "this."
+   
     const onTermSubmit = async(term) => {
         const response = await youtube.get('/search', {
             params: {
                 q: term
             }
         });
-        // update the state
-        // split the setState() into 2, one for setVideo response, one for setSelectedVideo response
-        // setState({
-        //     videos: response.data.items,
-        //     selectedVideo: response.data.items[0]
-        // })
+    
         setVideos(response.data.items);
         setSelectedVideo(response.data.items[0]);
     };
-    //6 cut n paste the onVideoSelect callback here to fire when the user clicks a video
-    // add const in front of onVideoSelect
-    const onVideoSelect = (video) => {
-            // replace this line as ff and prodie it with the video we want to select
-            // this.setState({ selectedVideo: video });
-            setSelectedVideo(video);
-        }
-        // 7. cut the entire return() part of render(), past here
+    // this is a one line func, we should call it inline in VideoList where it is called and delete this
+    // const onVideoSelect = (video) => {
+    //         setSelectedVideo(video);
+    //     }
+        
     return (
-        // add the callback as props then go add it to SearchBar.js
+        
         <div>
             <SearchBar onFormSubmit = { onTermSubmit } />  
              
@@ -55,7 +47,13 @@ const App = () => {
                     <div className = "five wide column" > 
                         
                         <VideoList 
-                        onVideoSelect = { onVideoSelect } 
+                        //1. rather than provide a separate func, we should make it an inline func
+                        // onVideoSelect = { onVideoSelect } 
+                        // onVideoSelect = {(video) => setSelectedVideo(video) }
+
+                        //2. again notice that we are providing one agr (video) and providing the same arg to setSelectedVideo(video) in our func. Anytime we set something like this in the same order it is a sign that we should do a little refactor
+                        // we can simply replace this func with this. It is 100% equiv to what we had b4
+                        onVideoSelect={ setSelectedVideo }
                         videos = { videos } /> 
                     </div> 
                 </div> 
