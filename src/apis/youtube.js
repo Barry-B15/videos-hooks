44121047b95c1e4e1ade5301f8c25926c4f55e9a
Barry <Barry-B15@users.noreply.github.com>
2021-07-14 23:29:35 +0900

    minor edit

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -3,7 +3,6 @@ import axios from "axios";
 // youtube api
 const KEY = "AIzaSyDt-LZtDr2nz5gP5GTL3NX7kYA0-IEZwzk";
 
-
 // create a preconfigured instance of axios
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3', //GET Request fro youtube doc
